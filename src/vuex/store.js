b460f1627531e76195a1050c1093cefd02cf98de
Nastya Borisova <nastasiia306@gmail.com>
2022-03-08 05:44:50 +0300

    refactor: vuex- divide store into modules

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -1,37 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import axios from 'axios'
+import products from "@/vuex/modules/products";
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-    products: [],
-  },
-  getters: {
-    PRODUCTS(state) {
-      return state.products;
-    }
-  },
-  mutations: {
-    SET_PRODUCTS_TO_STATE: (state, products) => {
-      state.products = products;
-    }
-  },
-  actions: {
-    GET_PRODUCTS_FROM_API({commit}) {
-      return axios('http://localhost:3000/products', {
-        method: 'GET',
-      })
-        .then((products) => {
-          commit('SET_PRODUCTS_TO_STATE', products.data);
-          return products;
-        })
-        .catch(error => {
-          console.log('test: ', error);
-          return error;
-        })
-    },
-  },
-  modules: {}
+  modules: {
+    products,
+  }
 });
